df35b20b02ab0619a3b66faad662c5300ccae3a1
PH-Lucotte <paul-henri.lucotte@insa-lyon.fr>
2025-01-15 10:24:48 +0100

    ajout cost

--- ospf.py
+++ ospf.py
@@ -1,11 +1,12 @@
 import json
 
-def config_ospf(router_id, process_id, neighbor_list):
+def config_ospf(router_id, process_id, neighbor_list, cost=0):
     """
     Args:
         str: l'id du routeur
         int: l'id du process
         list: la liste des voisins (rangée dans l'ordre des numéros d'interface)
+        int: la métrique du lien (0 si valeur pas défaut)
 
     Returns:
         list: liste des commandes pour configurer OSPF sur les routeurs
@@ -23,11 +24,15 @@ def config_ospf(router_id, process_id, neighbor_list):
             interface_name = "FastEthernet 0/0"
             commands.append(f"interface {interface_name}")
             commands.append(f"ipv6 ospf {process_id} area 0")
+            if cost:
+                commands.append(f"ipv6 ospf cost {cost}")
             commands.append("exit")
         else:
             interface_name = f"GigabitEthernet {i-1}/0"
             commands.append(f"interface {interface_name}")
             commands.append(f"ipv6 ospf {process_id} area 0")
+            if cost:
+                commands.append(f"ipv6 ospf cost {cost}")
             commands.append("exit")
 
     return commands
