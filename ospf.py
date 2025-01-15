29aa82f11728c2e75cc5af8c03d905fc91631549
PH-Lucotte <paul-henri.lucotte@insa-lyon.fr>
2025-01-15 09:30:35 +0100

    ajout exit

--- ospf.py
+++ ospf.py
@@ -20,13 +20,15 @@ def config_ospf(router_id, process_id, neighbor_list):
     # Generate interface-specific OSPF commands for neighbor_list
     for i, neighbor in enumerate(neighbor_list):
         if i == 0:
-            interface_name = f"FastEthernet 0/0"
+            interface_name = "FastEthernet 0/0"
             commands.append(f"interface {interface_name}")
             commands.append(f"ipv6 ospf {process_id} area 0")
+            commands.append("exit")
         else:
             interface_name = f"GigabitEthernet {i-1}/0"
             commands.append(f"interface {interface_name}")
             commands.append(f"ipv6 ospf {process_id} area 0")
+            commands.append("exit")
 
     return commands
 
