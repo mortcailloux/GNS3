7d50b2544e77f28c79ecca794d5e0dbe3b5ff5fc
cb <charlesbouquet1011@gmail.com>
2025-01-18 12:02:55 +0100

    recuperation configuration

--- test_fonctions.py
+++ test_fonctions.py
@@ -15,14 +15,16 @@ config_noeux=ad.attribue_ip(graphe,config_noeux)
 print(config_noeux)
 id.config_router_id(graphe,config_noeux)
 commande=ad.genere_commandes_ip(config_noeux,"R1")
-commande+=bgp.config_bgp_routeur("R1",graphe,config_noeux["R1"]["router_id"],config_noeux)
+#commande+=bgp.config_bgp_routeur("R1",graphe,config_noeux["R1"]["router_id"],config_noeux)
 #commande+=rip.config_rip_routeur("R1",graphe)
-commande+=ospf.config_ospf("1.1.1.1","R1",5,graphe,str(bgp.get_as_for_router("R1",graphe)),1) #attention pour accéder à la clé 
-commande+=loopback.generer_loopback_commandes("R1","ospf")
+#commande+=ospf.config_ospf("1.1.1.1","R1",5,graphe,str(bgp.get_as_for_router("R1",graphe)),1) #attention pour accéder à la clé 
+
+#commande+=loopback.generer_loopback_commandes("R1","ospf")
 print(commande)
-commande.append("write")
+
 telnet.recupérer_jsongns3_routeur(config_noeux)
 port=config_noeux["R1"]["json_gns3"].console
 print(port)
-telnet.configure_router_telnet("127.0.0.1",port,commande)
+config=telnet.configure_router_telnet("127.0.0.1",port,commande)
+print(config)
 
